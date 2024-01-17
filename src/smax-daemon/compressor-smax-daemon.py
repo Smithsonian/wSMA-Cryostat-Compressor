bcaacfef181d18de35fb8ae0fdf0d3b7d8fb4e88
Paul Grimes <pgrimes@cfa.harvard.edu>
2024-01-16 15:15:00 -1000

    fix smax_table munging

--- src/smax-daemon/compressor-smax-daemon.py
+++ src/smax-daemon/compressor-smax-daemon.py
@@ -254,7 +254,7 @@ class CompressorSmaxService:
         for key in logged_data.keys():
             self.logger.info(f"key in logged_data.keys(): {key}")
             if ":" in key:
-                atab = ":".join([self.smax_key, key.split(":")[0:-1]])
+                atab = ":".join([self.smax_key].extend(key.split(":")[0:-1]))
                 skey = key.split(":")[-1]
             else:
                 atab = self.smax_key
